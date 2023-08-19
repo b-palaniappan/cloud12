c5f66df98f86bb08b7303aabb592eda9ef082de4
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2023-08-19 11:44:55 -0500

    Added tailwindcss and daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="font-sans text-3xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev" class="link" target="_blank">kit.svelte.dev</a> to read the documentation</p>
