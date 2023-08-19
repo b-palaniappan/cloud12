c9b2f7e23efbaac425f59d61e43652f1856e5417
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2023-08-19 14:11:34 -0500

    formatted

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-  import Footer from '$lib/components/Footer.svelte';
-  import Header from '$lib/components/Header.svelte';
-  import "../app.css";
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import '../app.css';
 </script>
 
 <svelte:head>
-  <title>Cloud12</title>
-  <meta name="description" content="Personal website" />
+	<title>Cloud12</title>
+	<meta name="description" content="Personal website" />
 </svelte:head>
 
 <div class="container mx-auto py-2">
-  <div class="pb-8">
-    <Header />
-  </div>
-  <slot />
-  <div class="pt-8">
-    <Footer />
-  </div>
+	<div class="pb-8">
+		<Header />
+	</div>
+	<slot />
+	<div class="pt-8">
+		<Footer />
+	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-  import Hero from '$lib/components/Hero.svelte'
+	import Hero from '$lib/components/Hero.svelte';
 </script>
 
 <Hero />
 <h1 class="font-sans text-3xl">Welcome to Cloud12</h1>
-<p>Visit <a href="https://kit.svelte.dev" class="link" target="_blank">kit.svelte.dev</a> to read the documentation</p>
+<p>
+	Visit <a href="https://kit.svelte.dev" class="link" target="_blank">kit.svelte.dev</a> to read the
+	documentation
+</p>
