fcc6fb3817d57038a0951c5bd3e8158f2c1707d9
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2023-09-04 00:44:56 -0500

    Added version number to footer

---
+++ src/lib/components/Version.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import fs from 'fs';
+
+  export function getVersionNumber() {
+    const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+    return packageJson.version;
+  }
+  const version = getVersionNumber();
+</script>
+<p>Version {version}.</p>
