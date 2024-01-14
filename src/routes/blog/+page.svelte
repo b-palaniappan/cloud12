7d388822d072cb6055da517f256e8d56195f9e14
Balasubramanian Palaniappan <bpalaniappan@jobcase.com>
2024-01-14 12:34:08 -0600

    Upgrade changes

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,6 @@
 <div class="flex justify-center">
 	<article class="prose max-w-fit">
-		<h1>Gugan said put something else</h1>
+		<h1>Benefits of Garlic</h1>
 		<p>
 			For years parents have espoused the health benefits of eating garlic bread with cheese to
 			their children, with the food earning such an iconic status in our culture that kids will
